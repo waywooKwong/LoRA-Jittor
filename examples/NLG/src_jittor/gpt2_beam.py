15c82d3d672a010bbf5b7fa64a47dcdef207805f
waywooKwong <2211992@mail.nankai.edu.cn>
2025-07-06 22:53:39 +0800

    fix(jittor): fix

--- examples/NLG/src_jittor/gpt2_beam.py
+++ examples/NLG/src_jittor/gpt2_beam.py
@@ -89,9 +89,10 @@ def print_args(args):
 def _reorder_cache(past: Tuple, beam_idx: Var) -> Tuple[Any, ...]:
     # Error: RuntimeError: mat1 and mat2 shapes cannot be multiplied ...
     # Debug: index_select 时，得到的张量形状和预期不一致（batch_size 维被 squeeze 掉了）
-    
+    # (2, batch_size, num_heads, seq_len, head_dim) -> 
     # return tuple(layer_past.index_select(1, beam_idx).contiguous().detach() for layer_past in past)
-    return tuple(layer_past.getitem(((slice(None),)*1)+(beam_idx,)) for layer_past in past)
+    # return tuple(layer_past.getitem(((slice(None),)*1)+(beam_idx,)) for layer_past in past)
+    return tuple(layer_past[:, beam_idx] for layer_past in past)
 
 
 def _calc_banned_ngram_tokens(
